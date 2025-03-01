6349be0165aed18a85ec90dfb0b1ca22074bd530
minhi0449 <minhi0449@gmail.com>
2025-03-01 14:39:44 +0900

    Char 문자열 타입

---
+++ study/src/ch02/sec03/CharExample.java
@@ -0,0 +1,25 @@
+package ch02.sec03;
+
+/*
+    날짜 : 2025.03.01 (토)
+    이름 : 김민희
+    내용 : 문자열 타입
+ */
+
+public class CharExample {
+
+    public static void main(String[] args) {
+        char c1 = 'A'; // 문자 저장
+        char c2 = 65; // 유니코드 직접 저장
+
+        char c3 = '가'; // 문자 저장
+        char c4 = 44032; // 유니코드 직접 저장
+
+        System.out.println("c1 : " + c1);
+        System.out.println("c2 : " + c2);
+        System.out.println("c3 : " + c3);
+        System.out.println("c4 : " + c4);
+
+    }
+
+}
