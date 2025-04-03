d0e1a6a49f2090942750590453c932af67c145b3
minhi0449 <minhi0449@gmail.com>
2025-04-04 01:17:43 +0900

    docs: 변수 값 저장, 업데이트 및 교환(swap) 예제 학습

--- java-study/src/test/test_01.java
+++ java-study/src/test/test_01.java
@@ -10,7 +10,47 @@ package test;
 
 public class test_01 {
     public static void main(String[] args) {
+        // 1. 데이터 10, 100, 100 을 변수 선언과 동시에 초기화
+        int data1 = 10;
+        int data2 = 100;
+        int data3 = 100;
 
+        System.out.println("---------------------------------------------------");
+        System.out.println("data1 : " + data1);
+        System.out.println("data2 : " + data2);
+        System.out.println("data3 : " + data3);
+        System.out.println("---------------------------------------------------");
+        System.out.println();
+
+        // 2. int ageBox 상자에 20을 담기
+        int ageBox = 20;
+        System.out.println("ageBox : " + ageBox);
+        System.out.println("---------------------------------------------------");
+        System.out.println();
+
+        // 2-1. ageBox 상자의 값을 30으로 변경하고 기존에 담겨있던 데이터를 다른 곳으로 옮기기
+        int oldAge = ageBox; // 기존 데이터를 저장하기 위한 변수
+        ageBox = 30; // ageBox 상자의 값을 30으로 변경
+        System.out.println("ageBox : " + ageBox);
+        System.out.println("Old age in a different place : " + oldAge);
+        System.out.println("---------------------------------------------------");
+        System.out.println();
+
+        // 3. int a = 1; int b = 3; 선언하고 a와 b에 담겨 있는 값을 서로 스위칭
+        int a = 1;
+        int b = 3;
+        System.out.println("Before switching - a (변경 전 'a'의 값) : " + a);
+        System.out.println("Before switching - b (변경 전 'b'의 값) : " + b);
+        System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - ");
+
+
+        // 임시 변수를 사용하여 값을 교환
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After switching - a (변경 후 'a'의 값) : " + a);
+        System.out.println("After switching - b (변경 후 'b'의 값) : " + b);
+        System.out.println("---------------------------------------------------");
 
     }
 
