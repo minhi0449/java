3f3fcc4588ab5fa0f9f7b0c1749065e05941bfe7
minhi0449 <minhi0449@gmail.com>
2025-04-04 00:57:39 +0900

    docs: 변수 이름 규칙과 예제 코드 정리

---
+++ java-study/src/test/test_01.java
@@ -0,0 +1,17 @@
+package test;
+
+/*
+    1. 데이터 10, 100, 100 을 변수 선언과 동시에 초기화를 하고 화면에 출력해 보세요.
+    2. int ageBox 상자에 20을 담아 보세요.
+    2-1. ageBox 상자의 값을 30으로 변경하고 기존에 담겨 있던 데이터를 다른 곳으로 옮겨 보세요.
+    3. int a = 1; int b = 3; 선언하고 a와 b에 담겨 있는 값을 서로 스위칭 해보세요.
+
+ */
+
+public class test_01 {
+    public static void main(String[] args) {
+
+
+    }
+
+}
