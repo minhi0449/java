add899d16da800feed881231ba9fa5bdd8379c6c
minhi0449 <minhi0449@gmail.com>
2025-04-06 22:24:58 +0900

    add datatype-2

---
+++ java-study/src/ch01/Datatype2.java
@@ -0,0 +1,16 @@
+package 햣h01;
+
+/*
+    문자 데이터 타입 char / 자료형의 크기는 2byte;
+ */
+
+public class Datatype2 {
+    // 코드의 진입점
+    public static void main(String[] args) {
+
+        // 자료형 중 --> 기본 자료형 ( char ) 확인
+        // 문자를 저장하기 위한 변수를 선언할 때 사용할 수 있다.
+        // char 타입에는
+
+    }
+}
