f2954bf8586fd3db632a20c55efbc9541e6301d1
minhi0449 <minhi0449@gmail.com>
2025-04-10 02:59:56 +0900

    feat: 기본 데이터 타입 연습문제 (정수/실수/문자/논리)

--- java-study/src/test/Test02.java
+++ java-study/src/test/Test02.java
@@ -4,6 +4,42 @@ public class Test02 {
     public static void main(String[] args) {
 
         // 1. 정수 550 을 miniBox1 이름으로 변수에 담아 보세요.
+        int miniBox1 = 550;
+
+        // 2. 정수 22억을 miniBox2 이름으로 변수에 담아 보세요.
+        long miniBox2 = 2200000000L;
+
+        // 3. 문자 A를 miniBox3 이름으로 변수에 담아 보세요.
+        char miniBox3 = 'A';
+
+        // 4. 실수 float 타입으로 0.12345678을 miniBox4 이름으로 변수에 담아 보세요.
+        float miniBox4 = 0.12345678f;
+
+        // 5. 실수 double 타입으로 200.0035을 miniBox5 이름으로 변수에 담아 보세요.
+        double miniBox5 = 200.0035;
+
+        // 6. 논리 자료형 isOk 이름의 변수를 선언해 보세요.
+        boolean isOk;
+
+        // 7. isOk 변수의 값을 false 로 초기화 해보세요.
+        isOk = false;
+
+        // 8. isOk 변수의 값을 true 로 변경해 보세요.
+        isOk = true;
+
+        // 9. 변수란 무엇인지 서술해 보세요.
+        // -> 변할 수 있는 데이터(값)
+        // -> 값을 저장할 수 있는 메모리 공간(상자)
+
+        // 10. 기본 데이터 타입의 종류 8가지를 서술해 보세요.
+        // 표기방법 (byte, short, ...)
+        // 정수형 : int, long, byte, short
+        // 실수형 : double, float
+        // 문자형 : char
+        // 논리형 : boolean
+
+
+
 
 
     }
