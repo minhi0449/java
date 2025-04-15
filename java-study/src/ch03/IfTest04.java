26fc8bfd20ffefdb69883ef3c38390b3ab91dc30
minhi0449 <minhi0449@gmail.com>
2025-04-16 05:33:07 +0900

    chore: 조건문 개선 - 성적 범위(score 0~100) 따라 학점 분기 처리 및 예외 메시지 출력

--- java-study/src/ch03/IfTest04.java
+++ java-study/src/ch03/IfTest04.java
@@ -1,4 +1,28 @@
 package ch03;
 
+import java.util.Scanner;
+
 public class IfTest04 {
-}
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("성적 입력 : ");
+        int score = sc.nextInt();
+        System.out.println("score : " + score);
+
+        if (score >= 90 && score <= 100) {
+            System.out.println("A 학점입니다.");
+        } else if (score >= 80 && score < 90) {
+            System.out.println("B 학점입니다.");
+        } else if (score >= 70 && score < 80) {
+            System.out.println("C 학점입니다.");
+        } else if (score >= 60 && score < 90) {
+            System.out.println("D 학점입니다.");
+        } else if (score >= 0 && score < 60) {
+            System.out.println("F 학점입니다.");
+        } else if (score < 0 || score > 100) {
+            System.out.println("0 ~ 100 사이의 숫자를 입력해 주세요.");
+        }
+
+    } // end of main
+} // end of class
