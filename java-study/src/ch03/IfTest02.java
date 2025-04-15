09ffed4cfa5a5ab750149c98a1a2f491911a345e
minhi0449 <minhi0449@gmail.com>
2025-04-16 02:29:51 +0900

    feat: 조건 분기 학습을 위한 if-else 문 예제 추가

--- java-study/src/ch03/IfTest02.java
+++ java-study/src/ch03/IfTest02.java
@@ -1,4 +1,23 @@
 package ch03;
 
+/*
+    [if - else 문] 문법 (반드시 수행)
+
+    if(조건식) {
+        수행문1; // 조건식이 '참'인 경우에 수행됨
+    } else {
+        수행문2; // 조건식이 '참'이 아닌 경우에 수행됨
+    }
+
+ */
 public class IfTest02 {
+    public static void main(String[] args) {
+        int age = 10;
+        // if else -> 반드시 하나는 수행이 된다.
+        if (age >= 20){
+            System.out.println("쫌 아저씨네요!@");
+        }else {
+            System.out.println("아직 쫌 어리네요!");
+        }
+    }
 }
