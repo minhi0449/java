bd1787f026e281d6db75662a2d5952f617deeb95
minhi0449 <minhi0449@gmail.com>
2025-04-19 07:15:47 +0900

    feat: 객체 지향 프로그래밍(OOP)의 기본 속성을 가진 Student 클래스 생성

---
+++ java-study/src/ex_05/Student.java
@@ -0,0 +1,16 @@
+package ex_05;
+
+// 클래스란 ? 객체를 만들기 전 설계도면입니다.
+// 컴파일 타임 : 코드를 작성하고 javac 명령어 실행 시점까지
+// compile time : 소스 코드를 기계어로 변환하는 과정까지의 시간
+// 런타임 : java 명령어를 통해 프로그램이 실행 시점 부터 프로그램 종료 시점 까지
+// runtime : 프로그램이 실제 실행되는 동안의 시간
+public class Student {
+
+    String name;    // 학생 이름 (문자열)
+    int grade;      // 학년
+    String major;   // 학과
+    double height;  // 학생의 키
+    double weight;  // 학생의 몸무게
+
+} // end of class
