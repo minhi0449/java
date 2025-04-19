484a7374bb304ef2108fa988aa36ae6ae6555de3
minhi0449 <minhi0449@gmail.com>
2025-04-20 00:46:01 +0900

    feat: Bus 클래스 - 버스 초기화를 위한 생성자 구현 & Method(요금 수납 기능/상태 정보 표시)

--- java-study/src/ch10/Bus.java
+++ java-study/src/ch10/Bus.java
@@ -1,4 +1,31 @@
 package ch10;
 
+// 객체지향이란?
+// 객체와 객체 간의 관계를 형성하고 상호작용하게 코드를 작성하는 것
 public class Bus {
+
+    int busNumber; // 버스 호선
+    int count;     // 버스 승객 수
+    int money;     // 버스 수익금
+
+    // 생성자
+    public Bus(int number){
+        busNumber = number;
+        // 객체 생성 시에 제일 먼저 실행되는 부분
+    }
+
+    // 승객을 태우다.
+    public void take(int pay){
+        // money = money + pay;
+        money += pay;
+        count++;
+    }
+
+    public void showInfo(){
+        System.out.println("- - - - 상태창 - - - -");
+        System.out.println("버스 호선 : " + busNumber);
+        System.out.println("버스 승객 수 : " + count);
+        System.out.println("버스 수익금 : " + money);
+    }
+
 }
