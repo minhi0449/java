26f4128c0cc3be2939e2aa0f668a914275b27c83
minhi0449 <minhi0449@gmail.com>
2025-04-19 09:57:34 +0900

    feat: 도서(Book) 클래스 생성 및 도서 관련 속성 필드 정의 (title, author 등)

--- java-study/src/ex_05/Book.java
+++ java-study/src/ex_05/Book.java
@@ -1,4 +1,13 @@
 package ex_05;
 
 public class Book {
+    // 필드 (멤버 변수 - 속성)
+    String title;
+    String author;
+    int publishYear;
+    boolean isAvailable; // 책의 대출 가능 여부
+    int totalPages; // 책의 최종 페이지 수
 }
+
+
+
