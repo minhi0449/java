62d851b4dcb4df189227bcbbb176d89c3f8d12ff
minhi0449 <minhi0449@gmail.com>
2025-04-20 00:45:39 +0900

    feat: Bus 클래스 생성자 구현 및 테스트 코드 추가

--- java-study/src/ch09/BusMainTest.java
+++ java-study/src/ch09/BusMainTest.java
@@ -1,4 +1,20 @@
 package ch09;
 
 public class BusMainTest {
+    public static void main(String[] args) {
+
+        // 생성자를 사용하는 목적은
+        // 강제성을 부여하기 위함이다.
+        Bus b1 = new Bus(133, "시내버스", "미니");
+        b1.showInfo();
+
+        /*
+            ✓ 정리
+            • 기본 생성자 (컴파일러 자동 생성)
+            • 사용자 정의 생성자
+            • 생성자 여러개라면 : 생성자 오버로딩
+            • 생성자 객체가 생성될 때, 가장 먼저 수행되는 코드이다.
+         */
+
+    }
 }
