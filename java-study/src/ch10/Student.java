df75bb7d5b9ce0ddc27ac6e1c13c6cab49fbe17a
minhi0449 <minhi0449@gmail.com>
2025-04-20 00:46:24 +0900

    feat: Student 클래스와 버스 탑승 기능 구현

--- java-study/src/ch10/Student.java
+++ java-study/src/ch10/Student.java
@@ -1,4 +1,19 @@
 package ch10;
 
 public class Student {
+
+    String name; // 이름
+    int money;   // 소지금
+
+    // 매개변수 2개를 받는 생성자를 만들어 보자.
+    public Student(String n, int m){
+        name = n;
+        money = m;
+    }
+
+    // 학생이 버스를 탄다.
+    public void takeBus(Bus bus){
+
+    }
+
 }
