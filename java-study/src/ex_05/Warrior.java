592672b0b10952a1a5fcaca8aaa740bd4413a0fe
minhi0449 <minhi0449@gmail.com>
2025-04-19 09:58:30 +0900

    feat: 전사(Warrior) 클래스 생성 및 기본 속성 정의

--- java-study/src/ex_05/Warrior.java
+++ java-study/src/ex_05/Warrior.java
@@ -1,4 +1,13 @@
 package ex_05;
 
 public class Warrior {
+
+    // 전사의 속성들
+    String name;
+    int health;
+    int attackPower;    // 공격력
+    int defensePower;   // 방어력
+    double height;
+    double weight;
+    String color;
 }
