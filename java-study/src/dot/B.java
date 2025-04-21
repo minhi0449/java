accf26b1c6c49095d113f5342af7b341fdb31187
minhi0449 <minhi0449@gmail.com>
2025-04-21 17:30:02 +0900

    feat: B 클래스 - A 내부에 들어가는 객체, 내부에 C 객체를 가짐

--- java-study/src/dot/B.java
+++ java-study/src/dot/B.java
@@ -1,12 +1,18 @@
 package dot;
 
+// A 내부에 들어가는 객체, 내부에 C 객체를 가짐
 public class B {
 
+    // B 클래스는 C 타입의 필드 c를 가짐 (C는 B의 구성요소)
     C c;
 
     // 생성자
+    // B의 생성자 - A 생성자에서 B가 생성될 때, 자동 호출됨
     public B() {
+        System.out.println("여기는 public B() -> B 생성자 호출");
         // 무언가 코드를 작성해야 해 !
+        // C 객체도 생성하여 c 에 넣어줌
+        c = new C();
     }
 
 }
