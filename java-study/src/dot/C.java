968b064cd5228e1ca5a0403551f7d3e63c58263e
minhi0449 <minhi0449@gmail.com>
2025-04-21 17:30:20 +0900

    feat: C 클래스 - 최종적으로 callFinal() 메서드를 호출하는 객체

--- java-study/src/dot/C.java
+++ java-study/src/dot/C.java
@@ -1,9 +1,11 @@
 package dot;
 
+// C 클래스 : 최종적으로 callFinal() 메서드를 호출하는 객체
 public class C {
 
+    // callFinal 이라는 메서드는 단순히 출력만 함
     public void callFinal() {
-        System.out.println("callFinal() 메소드가 호출 되었다.");
+        System.out.println("여기는 C 클래스 -> callFinal() 메소드가 호출 되었다.");
     }
 
 }
