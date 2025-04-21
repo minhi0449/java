4e88b9b1a24f4c438119dcea8f6efe5637eba972
minhi0449 <minhi0449@gmail.com>
2025-04-21 16:15:43 +0900

    feat: A 클래스 생성 (최상위 객체, B 객체 포함)

---
+++ java-study/src/dot/A.java
@@ -0,0 +1,12 @@
+package dot;
+
+public class A {
+
+    B b;
+
+    // 생성자 (인스턴스 화 될 때 - 제일 먼저 수행하는 코드이다.)
+    public A(){
+        System.out.println("A 생성자 호출");
+        b = new B();
+    }
+}
---
+++ java-study/src/dot/B.java
@@ -0,0 +1,4 @@
+package dot;
+
+public class B {
+}
