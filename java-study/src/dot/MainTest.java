1dcb8c49bdd17371496f4258a934010c7c4bc7fb
minhi0449 <minhi0449@gmail.com>
2025-04-21 16:19:19 +0900

    feat: MainTest → A → B → C 구조 연결 및 callFinal() 호출 흐름 구성

--- java-study/src/dot/MainTest.java
+++ java-study/src/dot/MainTest.java
@@ -1,4 +1,14 @@
 package dot;
 
 public class MainTest {
+
+    public static void main(String[] args) {
+        System.out.println(" . 연산자의 여행 !!! ");
+
+        A a1 = new A();
+
+        a1.b.c.callFinal();
+
+    }
+
 }
