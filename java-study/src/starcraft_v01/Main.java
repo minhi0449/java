a5298d09f9fa28e2249fdf285bd360bf86145e9d
minhi0449 <minhi0449@gmail.com>
2025-04-23 17:50:35 +0900

    feat: Implement basic unit classes
    스타크래프트 기본 유닛(질럿, 저글링, 마린) 클래스 구현
    - 각 유닛별 기본 속성 및 메소드 구현
    - 유닛 간 전투 시스템 구현
    - 기본 정보 표시 기능 추가

--- java-study/src/starcraft_v01/Main.java
+++ java-study/src/starcraft_v01/Main.java
@@ -1,4 +1,31 @@
 package starcraft_v01;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        // 각 유닛 생성
+        Zealot zealot1 = new Zealot("질럿1");
+        Zergling zergling1 = new Zergling("저글링1");
+        Marine marine1 = new Marine("마린1");
+
+        // 각 유닛 정보 출력
+        zealot1.showInfo();
+        zergling1.showInfo();
+        marine1.showInfo();
+
+        System.out.println("\n ---- 전투 시작 ---- \n");
+
+        // 공격 테스트
+        zealot1.attackZergling(zergling1);
+        zergling1.attackZealot(zealot1);
+        marine1.attackZealot(zealot1);
+
+        // 전투 후 상태 확인
+        System.out.println(" \n ---- 전투 후 상태 ---- \n");
+        zealot1.showInfo();
+        zergling1.showInfo();
+        marine1.showInfo();
+
+    }
 }
