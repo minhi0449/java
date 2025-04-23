54cd7632c0034c2076f0f39a67c4c3437b36ea7a
minhi0449 <minhi0449@gmail.com>
2025-04-23 17:56:04 +0900

    test: 학생이 버스를 타는 시나리오 테스트 추가 (Student takeBus() test case)

--- java-study/src/oop1/MainTest.java
+++ java-study/src/oop1/MainTest.java
@@ -1,4 +1,20 @@
 package oop1;
 
 public class MainTest {
+
+    public static void main(String[] args) {
+
+        // "김복남"이라는 이름과 10,000원을 가진 학생 객체 생성
+        Student s1 = new Student("김복남", 10_000);
+        Bus bus1 = new Bus(202); // 202번 버스 객체 생성
+        Bus bus2 = new Bus(415); // 415번 버스 객체 생성
+
+        // 학생(s1)이 415번 버스(bus2)를 탑승
+        s1.takeBus(bus2);
+
+        s1.showInfo();
+        System.out.println("------------");
+        bus2.showInfo();
+
+    }
 }
