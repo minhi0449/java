0c862381d2afab9db56e2eb2a416a000b76322d2
minhi0449 <minhi0449@gmail.com>
2025-05-12 23:58:54 +0900

    feat: StringTest2 예제에 불변 스트링 객체 생성 코드 추가

--- java-study-2/src/_string/StringTest2.java
+++ java-study-2/src/_string/StringTest2.java
@@ -7,5 +7,9 @@ public class StringTest2 {
         String str1 = new String("Hello");
         String str2 = new String("World");
 
+        // 원래 객체 주소가 생성 되어 있고, 내부 변수 값이 변경이 된다면
+        // String 클래스는 수정되지 않고 불변이다.
+
+
     }
 }
