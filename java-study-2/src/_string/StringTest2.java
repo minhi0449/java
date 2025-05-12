0a0f3aee6552965511db4e0ccdc1c0c36be05c35
minhi0449 <minhi0449@gmail.com>
2025-05-12 23:59:58 +0900

    feat: "불변성(immutable)" 개념을 설명하기 위한 예제

--- java-study-2/src/_string/StringTest2.java
+++ java-study-2/src/_string/StringTest2.java
@@ -9,7 +9,9 @@ public class StringTest2 {
 
         // 원래 객체 주소가 생성 되어 있고, 내부 변수 값이 변경이 된다면
         // String 클래스는 수정되지 않고 불변이다.
+        // 즉, 수정이 되면 새로운 String 객체를 만드는 동작으로 실행된다.
 
+        System.out.println("고유 주소 확인 : " );
 
     }
 }
