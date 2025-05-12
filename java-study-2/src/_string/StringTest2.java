508aa6bf56eec5c0669ead2db8618c1e7c3cd0e8
minhi0449 <minhi0449@gmail.com>
2025-05-12 23:52:27 +0900

    feat: add immutable string object example in StringTest2

---
+++ java-study-2/src/_string/StringTest2.java
@@ -0,0 +1,11 @@
+package _string;
+
+public class StringTest2 {
+    public static void main(String[] args) {
+
+        // 스트링 객체는 불변이다.
+        String str1 = new String("Hello");
+        String str2 = new String("World");
+
+    }
+}
