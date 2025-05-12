f7bcd1d51c90ecce846b07793a836fb009a5c4d7
minhi0449 <minhi0449@gmail.com>
2025-05-12 23:48:18 +0900

    feat: equals() 오버라이드 추가 (title & author 기준 비교)

--- java-study-2/src/_object/Book.java
+++ java-study-2/src/_object/Book.java
@@ -1,6 +1,7 @@
 package _object;
 
-import dot.B;
+
+import java.util.Objects;
 
 /*
     Object 최상위 클래스에 대한 이해
@@ -10,31 +11,79 @@ import dot.B;
  */
 public class Book {
 
-    private String title;
-    private String author;
+    private int bookTypeId;
+    private String title;  // 책 제목
+    private String author; // 저자
 
-    public Book(String title, String author) {
-        this.title = title;
+    public Book(int bookTypeId, String author, String title) {
+        this.bookTypeId = bookTypeId;
         this.author = author;
+        this.title = title;
     }
 
-    // showInfo();
     @Override
-    public String toString() {
-        return "Book{" +
-                "title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                '}';
-    }
+    public boolean equals(Object obj) {
+        if(obj instanceof Book) {
+            // BooK 타입이 맞다면 추가적으로 , 제목이 같다면 같은 책으로 판단하겠다
+            // this.제목 == obj.제목 && this.저자 == obj.저자
 
-//    public String toString() {
-//        return "title : " + title + " , " + "author : " + author;
+            if(this.title.equals(((Book) obj).title) && this.author.equals(((Book) obj).author)) {
+                return true;
+            } else {
+                return false;
+            } // end of &&
+        } else {
+            return false;
+        } // end of if(obj instanceof Book)
+
+    } // end of equals
+
+
+    // equals() 메서드를 정의할 때는 hashCode() 메서드 재정의 해주어야
+    // 의도하지 않은 동작을 막을 수 있다.
+
+
+//    자동완성 (cmd + n)
+//    @Override
+//    public boolean equals(Object o) {
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Book book = (Book) o;
+//        return bookTypeId == book.bookTypeId && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(bookTypeId, title, author);
 //    }
 
     // 테스트 코드
     public static void main(String[] args) {
-        Book book1 = new Book("데미안", "헤르만헤세");
-        System.out.println("book1.toString() : " + book1.toString());
-    }
+        // 물리적 객체 주소값은 다르지만 equals() 메서드를 재정의해서
+        // 논리적으로 책 이름과 저자 이름이 같으면 같은 녀석으로 바라보게
+        // 프로그램 만들기
 
-}
+        // 책 제목과 저자 이름이 같다면 논리적으로 같은 책으로 판별
+        Book book1 = new Book(1, "데미안", "헤르만헤세");
+        Book book2 = new Book(2, "데미안", "헤르만헤세");
+        Book book3 = new Book(3, "데미안", "헤르만헤세");
+        Book book4 = new Book(4, "데미안", "헤르만헤세");
+
+        String str = new String("데미안");
+
+        System.out.println();
+        System.out.println("book1.toString() : " + book1.toString());
+        System.out.println("book2.toString() : " + book2.toString());
+
+        boolean result1 = book1.equals(book2);
+        boolean result2 = book1.equals(str);
+        boolean result3 = book1.equals(book3);
+        boolean result4 = book1.equals(book4);
+
+        System.out.println("------------------------------------------------");
+        System.out.println("result1 : " + result1);
+        System.out.println("result2 : " + result2);
+        System.out.println("result3 : " + result3);
+        System.out.println("result4 : " + result4);
+
+    } // end of main
+} // end of class (Book)
