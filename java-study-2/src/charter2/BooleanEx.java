b8a67ce1eee0911abad0010f81543de9819dba18
minhi0449 <minhi0449@gmail.com>
2025-05-25 21:05:01 +0900

    feat: boolean 타입 변수 - true 값으로 초기화, 논리형 데이터 타입의 기본 사용법 학습용 코드

--- java-study-2/src/charter2/BooleanEx.java
+++ java-study-2/src/charter2/BooleanEx.java
@@ -1,4 +1,10 @@
 package charter2;
 
+// 논리형 연습
 public class BooleanEx {
+    public static void main(String[] args) {
+        boolean isMarried = true; // boolean 변수를 선언하고 초기화 (불리언 기본값 : false)
+        System.out.println("isMarried : " + isMarried);
+    }
 }
+
