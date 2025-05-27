7e58a5ea59155f5fd380c26633470440192189ee
minhi0449 <minhi0449@gmail.com>
2025-05-27 11:50:34 +0900

    feat: 각 등수별(G/S/B) 및 default(A) 메달 색상 지정 로직 구현(불필요한 조건문 x)

--- java-study-2/src/ifexample/SwitchCase.java
+++ java-study-2/src/ifexample/SwitchCase.java
@@ -1,4 +1,24 @@
 package ifexample;
 
-public class SwithCase {
+// switch-case 문 예제
+public class SwitchCase {
+    public static void main(String[] args) {
+        int ranking = 1;
+        char medalColor;
+
+        switch (ranking) {
+            case 1:
+                medalColor = 'G';
+                break;
+            case 2:
+                medalColor = 'S';
+                break;
+            case 3:
+                medalColor = 'B';
+                break;
+            default:
+                medalColor = 'A';
+        }
+        System.out.println(ranking + "등 메달의 색깔은 '" + medalColor + "' 입니다.");
+    }
 }
