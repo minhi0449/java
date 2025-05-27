ccfff6483be80f9fe31223f3eeb007a94d755aee
minhi0449 <minhi0449@gmail.com>
2025-05-27 11:52:00 +0900

    feat: 점수 구간별로 A~F 학점이 할당되도록 if-else 구조를 적용함

--- java-study-2/src/ifexample/IfExample3.java
+++ java-study-2/src/ifexample/IfExample3.java
@@ -1,4 +1,25 @@
 package ifexample;
 
+// 성적에 따라 학점 부여하기
 public class IfExample3 {
+    public static void main(String[] args) {
+        int score = 95;
+        char grade;
+
+
+        if (score >= 90){
+            grade = 'A';
+        } else if (score >= 80){
+            grade = 'B';
+        } else if (score >= 70) {
+            grade = 'C';
+        } else if (score >= 60) {
+            grade = 'D';
+        }
+        else {
+            grade = 'F';
+        }
+
+        System.out.println("grade는 '" + grade + "' 학점 입니다.");
+    }
 }
