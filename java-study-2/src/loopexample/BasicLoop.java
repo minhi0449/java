ad81aad924b39d8c658a7180272e9e2f64ec7ad4
minhi0449 <minhi0449@gmail.com>
2025-05-27 11:46:22 +0900

    feat: 복합 대입 연산자를 활용하여 1~10까지의 합을 구하는 로직 작성

--- java-study-2/src/loopexample/BasicLoop.java
+++ java-study-2/src/loopexample/BasicLoop.java
@@ -1,4 +1,20 @@
 package loopexample;
 
+// 1 부터 10 까지 더하기
 public class BasicLoop {
+    public static void main(String[] args) {
+        int num = 1;
+        num += 2; // 복합 대입 연산자 --> num = num + 2; 와 같음
+        num += 3;
+        num += 4;
+        num += 5;
+        num += 6;
+        num += 7;
+        num += 8;
+        num += 9;
+        num += 10;
+
+        System.out.println("1부터 10까지의 합은 '" + num + "' 입니다.");
+
+    }
 }
