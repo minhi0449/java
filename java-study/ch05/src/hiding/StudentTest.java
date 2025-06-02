5a5fd9deea342bc7d61476c02eee470af723852f
minhi0449 <minhi0449@gmail.com>
2025-06-02 15:42:43 +0900

    feat: private 변수 - 직접 접근 시 오류 발생 확인 및 우회 방법 검증

---
+++ java-study/ch05/src/hiding/StudentTest.java
@@ -0,0 +1,13 @@
+package hiding;
+
+// private 변수 테스트 하기
+public class StudentTest {
+    public static void main(String[] args) {
+        Student studentLee = new Student();
+        //studentLee.studentName = "이상원"; // 오류 발생
+        studentLee.setStudentName("이상원");
+        // setStudentName() 메서드를 활용해 private 변수에 접근 가능
+
+        System.out.println(studentLee.getStudentName());
+    }
+}
