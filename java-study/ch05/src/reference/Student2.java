856cf700af6690904e4bbc3675f5e1924c154411
minhi0449 <minhi0449@gmail.com>
2025-06-02 15:46:05 +0900

    feat: 과목명 저장할 변수 추가 → 데이터 표현력 확장

---
+++ java-study/ch05/src/reference/Student2.java
@@ -0,0 +1,12 @@
+package reference;
+
+// 학생 클래스 만들기(2)
+public class Student2 {
+    int studentID;
+    String studentName;
+    int koreaScore;
+    int mathScore;
+    // 과목 이름 변수 추가
+    String koreaSubject;
+    String mathSubject;
+}
