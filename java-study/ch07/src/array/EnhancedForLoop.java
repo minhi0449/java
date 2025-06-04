881c3ebf45b524d5eabe7e28bf33c540bcfa4d4a
minhi0449 <minhi0449@gmail.com>
2025-06-04 21:51:56 +0900

    feat: 향상된 for문(for-each) 구문을 활용한 배열 순회 방식을 구현

---
+++ java-study/ch07/src/array/EnhancedForLoop.java
@@ -0,0 +1,12 @@
+package array;
+
+// 향상된 for 문 사용하기
+public class EnhancedForLoop {
+    public static void main(String[] args) {
+        String[] strArray = {"Java", "Android", "C", "JavaScript", "Python" };
+
+        for (String lang : strArray) {
+            System.out.println(lang);
+        }
+    }
+}
