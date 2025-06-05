307a87cb6237b4adc91b7d66007a11358f8159a9
minhi0449 <minhi0449@gmail.com>
2025-06-05 22:33:33 +0900

    feat: for문을 사용한 1-10 누적합 계산 예제 구현

--- java-study-2/src/loopexample/ForExample1.java
+++ java-study-2/src/loopexample/ForExample1.java
@@ -1,4 +1,18 @@
 package loopexample;
 
+// for문 예제
 public class ForExample1 {
+    public static void main(String[] args) {
+        int i;
+        int sum;
+
+        // for 문에서 가장 자주 사용하는 변수 이름은 i
+        // 주로 횟수를 표현
+        for (i = 1, sum = 0; i <= 10; i++){
+            sum += i;
+        }
+
+        System.out.println("1부터 10까지의 합은 '" + sum + "' 입니다.");
+
+    }
 }
