9c498d99cb25d70e341812e0cc0d866449e99ffb
minhi0449 <minhi0449@gmail.com>
2025-06-05 22:37:59 +0900

    feat: 반복문 내에서 변수값과 문자열을 결합하여 동적 메시지 생성

--- java-study-2/src/loopexample/ForExample2.java
+++ java-study-2/src/loopexample/ForExample2.java
@@ -1,4 +1,12 @@
 package loopexample;
 
+// 나 혼자 코딩
+// for 문과 변수를 사용하여 '안녕하세요1~10' 까지 차례로 출력하는 프로그램
 public class ForExample2 {
+    public static void main(String[] args) {
+        int i;
+        for (i = 1; i <= 10; i++){
+            System.out.println("안녕하세요 " + i);
+        }
+    }
 }
