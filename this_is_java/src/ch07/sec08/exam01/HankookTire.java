34cdde91183d5e2de4bd2d2e247b345eaab73190
minhi0449 <minhi0449@gmail.com>
2025-06-10 14:23:36 +0900

    feat: 메서드 오버라이딩을 통한 런타임 동적 바인딩 동작 확인

---
+++ this_is_java/src/ch07/sec08/exam01/HankookTire.java
@@ -0,0 +1,11 @@
+package ch07.sec08.exam01;
+
+public class HankookTire extends Tire{
+
+	@Override
+	public void roll() {
+		// TODO Auto-generated method stub
+		System.out.println("한국 타이어가 회전합니다.");
+	}
+	
+}
