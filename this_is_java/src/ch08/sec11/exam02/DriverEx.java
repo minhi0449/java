6c10e735319317839a8f514043ad0d010ac0f0a2
minhi0449 <minhi0449@gmail.com>
2025-06-15 14:49:14 +0900

    feat: instanceof 패턴 매칭으로 타입 안전하게 요금 체크 구현

---
+++ this_is_java/src/ch08/sec11/exam02/DriverEx.java
@@ -0,0 +1,13 @@
+package ch08.sec11.exam02;
+
+public class DriverEx {
+	public static void main(String[] args) {
+		Driver driver = new Driver();
+		
+		Bus bus = new Bus();
+		Taxi taxi = new Taxi();
+		
+		driver.driver(bus);
+		driver.driver(taxi);
+	}
+}
