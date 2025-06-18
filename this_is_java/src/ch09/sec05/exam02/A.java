01af77b6f13195a8e8a053bcf5039cd405dd9353
minhi0449 <minhi0449@gmail.com>
2025-06-18 21:08:01 +0900

    feat: add inner class B with access to outer class A

---
+++ this_is_java/src/ch09/sec05/exam02/A.java
@@ -0,0 +1,14 @@
+package ch09.sec05.exam02;
+
+public class A {
+	String field = "A-field";
+	
+	void method() {
+		System.out.println("A method");
+	}
+	
+	// A의 인스턴스 메소드
+	void useB() {
+		
+	}
+}
