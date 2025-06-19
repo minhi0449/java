bb90eec05e58d488ff5d7088da42cae666fabef9
minhi0449 <minhi0449@gmail.com>
2025-06-19 23:23:47 +0900

    feat: 정적 중첩 인터페이스를 활용한 버튼 이벤트 처리 구현

---
+++ this_is_java/src/ch09/sec06/exam03/Button.java
@@ -0,0 +1,24 @@
+package ch09.sec06.exam03;
+
+public class Button {
+	// 정적 중첩 인터페이스
+	public static interface ClickListener {
+		// 추상 메소드
+		void onClick();
+	}
+	
+	private ClickListener clickListener;
+
+	public ClickListener getClickListener() {
+		return clickListener;
+	}
+
+	public void setClickListener(ClickListener clickListener) {
+		this.clickListener = clickListener;
+	}
+	
+	public void click() {
+		this.clickListener.onClick();
+	}
+	
+}
