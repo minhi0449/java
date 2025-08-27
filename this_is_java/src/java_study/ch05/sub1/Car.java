c05217a2b9bc90134d2c16ef4ac30184c3c218f9
minhi0449 <minhi0449@gmail.com>
2025-08-28 00:21:09 +0900

    java

---
+++ this_is_java/src/java_study/ch05/sub1/Car.java
@@ -0,0 +1,25 @@
+package java_study.ch05.sub1;
+
+// 클래스 정의
+public class Car {
+	// 속성(멤버변수)
+	String name; // 변수 선언
+	String color;
+	int speed;
+	
+	// 기능 
+	public void speedUp(int speed) {
+		// this 클래스를 지칭하는 키워드 (int speed 를 가리킴)
+		this.speed += speed;
+	}
+	
+	public void speedDown(int speed) {
+		this.speed -= speed;
+	}
+	
+	public void show() {
+		System.out.println("차량명 : " + this.name);
+		System.out.println("차량색 : " + this.color);
+		System.out.println("현재속도 : " + this.speed);
+	}
+}
