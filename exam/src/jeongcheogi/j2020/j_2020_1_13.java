f907ac4dccf02dda20cd2da1f41bf46287a5532b
minhi0449 <minhi0449@gmail.com>
2025-09-26 00:31:52 +0900

    배열(Array)과 for 반복문을 이용한 순차 출력

---
+++ exam/src/jeongcheogi/j2020/j_2020_1_13.java
@@ -0,0 +1,13 @@
+package jeongcheogi.j2020;
+
+// 2020년 1회 기출문제 13번
+// 배열(Array)과 for 반복문을 이용한 순차 출력
+public class j_2020_1_13 {
+	public static void main(String[] args) {
+		int i;
+		int []a = {0, 1, 2, 3};
+		for(i = 0; i < 4; i++) {
+			System.out.print(a[i] + " ");
+		}
+	}
+}
