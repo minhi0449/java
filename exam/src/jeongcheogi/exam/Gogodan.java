073110f68fb6a64418fb8519d897802448a88b65
minhi0449 <minhi0449@gmail.com>
2025-12-17 23:53:34 +0900

    정보처리기사

---
+++ exam/src/jeongcheogi/exam/Gogodan.java
@@ -0,0 +1,14 @@
+package jeongcheogi.exam;
+
+public class Gogodan {
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.println(i + "x" + j + "=" + (i * j));
+            }
+            System.out.println();
+        }
+		
+	}
+}
